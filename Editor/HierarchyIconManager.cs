d919a04bebd8aec511b137027dfe7dbe2ccce64e
박기락 <kirak0622@gmail.com>
2023-04-09 13:24:50 +0900

    가즈아

---
+++ Editor/HierarchyIconManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+[InitializeOnLoad]
+public static class HierarchyIconManager
+{
+	static HierarchyIconManager()
+	{
+		Debug.Log("HierarchyIconManager!");
+		EditorApplication.hierarchyWindowItemOnGUI += DrawHierarchyIcon;
+	}
+
+	private static Dictionary<Type, Texture2D> iconCache = new Dictionary<Type, Texture2D>();
+
+	private static void DrawHierarchyIcon(int instanceID, Rect selectionRect)
+	{
+		if (!showIcons)
+			return;
+
+		GameObject go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+		if (go == null)
+			return;
+
+		var components = go.GetComponents<Component>();
+
+		float iconSize = 16f;
+		float initialXPosition = selectionRect.x + selectionRect.width;
+
+		foreach (var component in components)
+		{
+			if (component == null)
+				continue;
+
+			Type componentType = component.GetType();
+
+			if (!iconCache.TryGetValue(componentType, out Texture2D icon))
+			{
+				icon = EditorGUIUtility.ObjectContent(component, componentType).image as Texture2D;
+				iconCache[componentType] = icon;
+			}
+
+			if (icon != null)
+			{
+				Rect iconRect = new Rect(initialXPosition, selectionRect.y, iconSize, iconSize);
+				GUI.color = Color.white;
+				GUI.DrawTexture(iconRect, icon);
+				initialXPosition -= iconSize;
+			}
+		}
+	}
+
+	private static bool showIcons = true;
+
+	[MenuItem("HierarchyIcons/Show Icons")]
+	private static void ToggleShowIcons()
+	{
+		showIcons = !showIcons;
+		Menu.SetChecked("HierarchyIcons/Show Icons", showIcons);
+	}
+}
+
+#endif
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+# Custom Hierarchy
+
+![img.png](img.png)
+
+## 작성동기
+어느 오브젝트에 스크립트가 붙었는지, 살펴보는 시간이 킹받아서 제작하였음.
+#### #노안이슈 #손떨림이슈  #그냥 Aging이슈
+
+## 기능
+gameObject에 붙은 컴포넌트 Icon들을 나열함.
+
+## 사용 주의사항
+Editor 폴더안에 스크립트를 둘것.
+빌트인 아이콘이 아닌 아이콘을 사용하려면, Editor Default Resources 폴더를 활용할것.
+
+## 활용 아이디어들
+1. 컴포넌트에 노출되는 프로퍼티가 None일때, Icon색상을 Red로 세팅한다.
+- ex) Image 컴포넌트의 sprite가 None이거나, Text의 font가 None이거나, 
+
+2. Graphic를 상속받는 컴포넌트들의 raycast target값이 true,false에 따라, Icon색상을 세팅한다.
+3.  특정 컴포넌트들만 Icon나열한다.
+- ex) 컴포넌트가 많이 붙은상태일때, 보기가 힘듬.
+
